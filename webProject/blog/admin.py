cae1e3aef54c51ced08c4458e9c18f3282f8ab02
Guillermo Cacho <guillermo.cacho.v@gmail.com>
2022-09-03 20:40:55 -0600

    Blog Creado, migrado y conectado al panel de administrador

---
+++ webProject/blog/admin.py
@@ -0,0 +1,15 @@
+# Para mas info: https://youtu.be/ClbbhYN8_ec
+from django.contrib import admin
+from .models import Categoria, Post
+
+# Register your models here.
+
+class CategoriaAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+admin.site.register(Categoria, CategoriaAdmin)
+
+admin.site.register(Post, PostAdmin)
--- webProject/webProject/settings.py
+++ webProject/webProject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'webProject',
     'servicios',
+    'blog',
 ]
 
 MIDDLEWARE = [
