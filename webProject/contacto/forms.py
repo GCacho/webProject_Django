0f485798c7223f6d2031d03bacbd2f0ec8bd3fca
Guillermo Cacho <guillermo.cacho.v@gmail.com>
2022-09-04 14:56:14 -0600

    Aplicacion de contacto creada y conectada con formularios por metodo POST y GET

---
+++ webProject/contacto/forms.py
@@ -0,0 +1,7 @@
+# Para más info: https://youtu.be/C8cn-Z-Ps4A
+from django import forms
+
+class FormularioContacto(forms.Form):
+    nombre = forms.CharField(label="Nombre", required=True)
+    email = forms.EmailField(label="Email", required=True)
+    contenido = forms.CharField(label='Contenido', widget=forms.Textarea)
--- webProject/contacto/views.py
+++ webProject/contacto/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import FormularioContacto
 
 # Create your views here.
 def contacto(request):
-    return render(request,"contacto/contacto.html")
+    formulario_contacto= FormularioContacto()
+    if request.method=="POST":
+        formulario_contacto=FormularioContacto(data=request.POST) # Para mas info https://youtu.be/_BOOv6GK-68
+        if formulario_contacto.is_valid():
+            nombre=request.POST.get("nombre")
+            email=request.POST.get("email")
+            contenido=request.POST.get("contenido")
+            return redirect("/contacto/?valido")
+    return render(request,"contacto/contacto.html", {'miFormulario':formulario_contacto})
