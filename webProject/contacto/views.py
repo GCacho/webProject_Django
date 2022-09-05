ec4f65fa22bd6ba29e0fa44c573b3d40ae3e9681
Guillermo Cacho <guillermo.cacho.v@gmail.com>
2022-09-05 13:35:47 -0600

    Aplicación de contacto finalizada

--- webProject/contacto/views.py
+++ webProject/contacto/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from django.core.mail import EmailMessage
 
 from .forms import FormularioContacto
+
 
 # Create your views here.
 def contacto(request):
@@ -8,8 +10,21 @@ def contacto(request):
     if request.method=="POST":
         formulario_contacto=FormularioContacto(data=request.POST) # Para mas info https://youtu.be/_BOOv6GK-68
         if formulario_contacto.is_valid():
-            nombre=request.POST.get("nombre")
+            nombre=request.POST.get("nombre") # Los nombres vienen de forms.py
             email=request.POST.get("email")
             contenido=request.POST.get("contenido")
-            return redirect("/contacto/?valido")
+            
+            email=EmailMessage( # Para mas info: https://youtu.be/J0_RYvw3SlY
+                "Mensaje desde Django",
+                f"El usuario: {nombre} con la direccion: {email} envio el siguiente correo: {contenido}",
+                ["guillermo.cacho.v@gmail.com"],
+                reply_to=[email]
+            ) 
+            
+            try:
+                email.send()
+                return redirect("/contacto/?valido")
+            except:
+                return redirect("/contacto/?novalido")
+
     return render(request,"contacto/contacto.html", {'miFormulario':formulario_contacto})
--- webProject/webProject/settings.py
+++ webProject/webProject/settings.py
@@ -136,3 +136,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Conexion a Correo Electronico / Para mas info: https://youtu.be/J0_RYvw3SlY
+
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+# Tu mail : EMAIL_HOST_USER="TuCorreo"
+# EMAIL_HOST_PASSWORD="TuPassword" # Para Gmail, necesitas crear tu contraseña para aplicaciones de terceros / No me funciono xd
